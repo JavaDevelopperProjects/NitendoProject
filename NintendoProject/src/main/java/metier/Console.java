8dd68ebcf19c6593058dfdfd365eb31345a58e41
Le Nay Yann <yann.le.nay@gmail.com>
2021-05-10 18:16:06 +0200

    modif console

--- NintendoProject/src/main/java/metier/Console.java
+++ NintendoProject/src/main/java/metier/Console.java
@@ -3,23 +3,36 @@ package metier;
 public class Console {
 
 	private String nom;
-
-	public Console(String nom) {
+	private int prix;
+	private String date;
+	public Console(String nom, int prix, String date) {
 		this.nom = nom;
+		this.prix = prix;
+		this.date = date;
 	}
-
 	public String getNom() {
 		return nom;
 	}
-
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
+	public int getPrix() {
+		return prix;
+	}
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
 	@Override
 	public String toString() {
-		return "Console [nom=" + nom + "]";
+		return "Console [nom=" + nom + ", prix=" + prix + ", date=" + date + "]";
 	}
+
 	
 	
 }
